79b068f4defe3cf590ff9e1bacb4ede2172cd189
pc <pc@DESKTOP-CJPR87R>
2022-11-21 16:23:29 +0900

    Test2

---
+++ clientexample/src/main/java/clientexample/ClientExampleApplication.java
@@ -0,0 +1,13 @@
+package clientexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientExampleApplication.class, args);
+	}
+
+}
